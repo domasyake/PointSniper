9961a615a72343e564584231257ae5a5c0ae4134
domasyake <lehmboden.doma@gmail.com>
2019-01-15 01:07:36 +0900

    create:circle sniper

---
+++ Assets/Examples/ExampleScripts/CircleExample.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using PointSniper;
+using UnityEngine;
+
+namespace Examples.ExampleScripts{
+	public class CircleExample : BaseExample{
+		[SerializeField] private RectTransform center;
+		[SerializeField] private RectTransform radiusPoint;
+		[SerializeField] private RectTransform circle;
+		[SerializeField] private RectTransform lineStart;
+		[SerializeField] private RectTransform lineEnd;
+		
+
+		private CircleSniper circleSniper;
+		
+		
+		private void Update(){
+			var center_pos = Screen2WorldPoint(center);
+			var radius_point_pos = Screen2WorldPoint(radiusPoint);
+			
+			var world_radius = (float)Math.Sqrt(Math.Pow(radius_point_pos.x - center_pos.x, 2) +
+			                       Math.Pow(radius_point_pos.y - center_pos.y, 2));
+
+			Debug.DrawLine(center_pos,radius_point_pos,Color.blue);
+			Debug.DrawLine(center_pos,center_pos+new Vector3(world_radius,0),Color.green);
+			Debug.DrawLine(center_pos,center_pos+new Vector3(-world_radius,0),Color.green);
+			Debug.DrawLine(center_pos,center_pos+new Vector3(0,world_radius,0),Color.green);
+			Debug.DrawLine(center_pos,center_pos+new Vector3(0,-world_radius,0),Color.green);
+
+			var start_pos = Screen2WorldPoint(lineStart);
+			var end_pos=Screen2WorldPoint(lineEnd);
+			Debug.DrawLine(start_pos,end_pos,Color.cyan);
+			
+			var anchored_center=center.anchoredPosition;
+			var anchored_radius=radiusPoint.anchoredPosition;
+			var radius = (float)Math.Sqrt(Math.Pow(anchored_radius.x - anchored_center.x, 2) +
+			                       Math.Pow(anchored_radius.y - anchored_center.y, 2));
+
+			circle.sizeDelta = new Vector2(radius*2.22f, radius*2.22f);
+;			circleSniper=new CircleSniper(center.anchoredPosition,radius);
+			myPointSniper = circleSniper;
+		}
+
+		public void OnCross(){
+			icon.anchoredPosition = circleSniper.RandomSnipeOnCross();
+		}
+
+		public void GetLinePoint(){
+			var points = circleSniper.RandomSnipesToLine();
+			lineStart.anchoredPosition = points[0];
+			lineEnd.anchoredPosition = points[1];
+		}
+	}
+}
---
+++ Assets/Scripts/PointSniper/CircleSniper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace PointSniper{
+	public class CircleSniper:IPointSniper{
+
+		private readonly Vector2 center;
+		private readonly float radius;
+		
+		public CircleSniper(Vector2 center,float radius){
+			this.center = center;
+			this.radius = radius;
+		}
+		
+		
+		public Vector3 CenterSnipe(){
+			return center;
+		}
+
+		public Vector3 RandomSnipe(){
+			var rad=Random.Range(0,360f)*Mathf.Deg2Rad;
+			var range = Random.Range(0, radius);
+			return new Vector3(center.x+range*Mathf.Sin(rad),center.y+range*Mathf.Cos(rad));
+		}
+
+		public Vector3 RandomSnipeOnLine(){
+			var rad=Random.Range(0,360f)*Mathf.Deg2Rad;
+			return new Vector3(center.x+radius*Mathf.Sin(rad),center.y+radius*Mathf.Cos(rad));
+		}
+
+		public Vector3 RandomSnipeOnCross(float rotation_angle=0){
+			var rad = 0f;
+			var random = Random.Range(0, 4);
+			switch (random){
+				case 0:
+					rad = 90;
+					break;
+				case 1:
+					rad = 180;
+					break;
+				case 2:
+					rad = 270;
+					break;
+			}
+
+			rad = (rad+rotation_angle)* Mathf.Deg2Rad;
+			var range = Random.Range(0, radius);
+			return new Vector3(center.x+range*Mathf.Sin(rad),center.y+range*Mathf.Cos(rad));
+		}
+
+		public Vector3[] RandomSnipesToLine(){
+			var start_point = RandomSnipeOnLine();
+			var end_point=new Vector3(center.x*2-start_point.x,center.y*2-start_point.y);
+			return new Vector3[]{start_point,end_point};
+		}
+	}
+}
